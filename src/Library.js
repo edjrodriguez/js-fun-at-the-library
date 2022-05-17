434108250cd98943cd22ca1fce25466e58b8a10c
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-05-16 19:04:30 -0600

    Initial commit

--- src/Library.js
+++ src/Library.js
@@ -11,15 +11,15 @@ function createLibrary (libraryName) {
 
 // this addBook function will add to only 3 specific shelves: fantasy, nonFiction, fiction.  Not a dynamic function.
 
-// function addBook (libraryName, bookTitle) {
-//   if (bookTitle.genre.includes("fantasy")){
-//     return libraryName = libraryName.shelves.fantasy.unshift(bookTitle)
-//   } else if (bookTitle.genre.includes("nonFiction")) {
-//     return libraryName = libraryName.shelves.nonFiction.unshift(bookTitle)
-//   } else if (bookTitle.genre.includes("fiction")) {
-//     return libraryName = libraryName.shelves.fiction.unshift(bookTitle)
-//   }
-// };
+function addBook (libraryName, bookTitle) {
+  if (bookTitle.genre.includes("fantasy")){
+    return libraryName = libraryName.shelves.fantasy.unshift(bookTitle)
+  } else if (bookTitle.genre.includes("nonFiction")) {
+    return libraryName = libraryName.shelves.nonFiction.unshift(bookTitle)
+  } else if (bookTitle.genre.includes("fiction")) {
+    return libraryName = libraryName.shelves.fiction.unshift(bookTitle)
+  }
+};
 
 
 // this addBook function is  dynamic and will create a shelf of any genre that a book hapens to be.  No need to specify genres in this function.
--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,36 @@
 class Librarian {
-  constructor() {
-    
+  constructor(firstName, library) {
+    this.name = firstName;
+    this.library = library;
   }
-}
+  greetPatron(patronName, morning) {
+    if ((patronName) && (morning) == (patronName && true)){
+      return `Good morning, ${patronName}!`;
+    } else ((patronName) && (morning) == (patronName && false))
+    return `Hello, ${patronName}!`;
+  }
+
+  // findBook(onBookShelf, book) {
+  //   var onBookShelf = `Yes, we have ${book}`;
+  //   var checkedOut = `Sorry, we do not have ${book}`;
+  //
+  //   for (var i = 0; i < onBookShelf.length; i++){
+  //     if (onBookShelf.library == (book)
+  //     return onBookShelf;
+    // } else {
+    //   return checkedOut;
+  
+  };
+
+
+
+
+//
+//     if(patronName && timeOFDay === patronName && true) {
+//     return `Good morning, ${patronName}!`;
+//   } else if(patronName && timeOFDay === patronName && false) {
+//     return `Hello, ${patronName}!`;
+//   }
+// }
 
 module.exports = Librarian;
